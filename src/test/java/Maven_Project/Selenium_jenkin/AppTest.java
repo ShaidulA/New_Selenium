1f2a05a0bd9db618e52aacfa363308d825a1c532
shidul <shaidulazam@gmail.com>
2024-02-18 10:04:43 -0500

    Updated

--- src/test/java/Maven_Project/Selenium_jenkin/AppTest.java
+++ src/test/java/Maven_Project/Selenium_jenkin/AppTest.java
@@ -1,20 +1,57 @@
 package Maven_Project.Selenium_jenkin;
 
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
-	
-	public void setup()
+	WebDriver driver;
+	@BeforeMethod
+	public void browser() 
 	{
-		WebDriver driver=new ChromeDriver();
-		driver.manage().window().maximize();
 		
-		
+	driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://www.orangehrm.com/");
+	driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+	
 	}
-}
+	@AfterMethod
+	public void teardown()
+	{
+		driver.close();
+	}
+
+
+
+	
+	@Test
+	  public void login () 
+	  {
+		  System.out.println("login");
+		  //Assert.assertEquals("true")
+		  
+	  }
+	  
+	  @Test(dependsOnMethods="login")
+	  public void application () 
+	  {
+		  System.out.println("application");
+	  }
+
+	  @Test
+	  public void logout() 
+	  {
+		  System.out.println("logout");
+		  
+	  }
+	}
